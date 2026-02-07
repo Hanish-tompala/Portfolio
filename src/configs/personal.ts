55078efe03154a991cf71a9cefcf6d34b695be93
Lucifer <103019796+Hanish-tompala@users.noreply.github.com>
2026-02-07 22:04:53 +0530

    Update personal.ts

--- src/configs/personal.ts
+++ src/configs/personal.ts
@@ -7,7 +7,7 @@ export const personalInfo = {
   website: "",
   resumeUrl: "/resume.pdf",
   avatar: "/photo.jpeg",
-  bio: `I’m a first-year Computer Science student with a massive interest in Cybersecurity and IoT. Right now, I’m focusing on building my foundation in Python and web development while learning how to secure connected systems. When I’m not in class, I’m usually managing the Techverse cybersecurity group or experimenting with Kali Linux to understand how things work under the hood.`,
+  bio: `I’m a first-year Computer Science student with a massive interest in Cybersecurity and IoT. Right now, I’m focusing on building my foundation in Python and web development while learning how to secure connected systems. When I’m not in class, I’m experimenting with Kali Linux to understand how things work under the hood.`,
   highlights: [
     "Active Open Source Contributor",
   ],
